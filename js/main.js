15120da79c2becbd0605ff6eba028f90b663ce3b
Callum.v.H <callumvanheerden@gmail.com>
2021-10-15 22:59:38 +0000

    added rest

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let tasks = []
+let completedTasks =[]
+
+createElement(type, parentEl);
+function createElement() {
+    let element = document.createElement(type);
+    // document.body.appendChild(input);
+    document.parentEl.appendChild(element)
+}
+
+createElement("input", body)
+
